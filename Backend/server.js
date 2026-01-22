775c13a62b6331a474430349667f5e50f012a09f
Jahangeer Haider <154549449+JahangeerHaider@users.noreply.github.com>
2026-01-22 12:01:59 +0500

    resetting port

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(process.env.PORT, () => {
-  console.log(`server is runing on port ${PORT}`);
- });
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
